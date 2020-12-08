ee5d8d09d6062958c4ce1816f782b159571c297d
ryandeering <ryandeering1@gmail.com>
2020-12-08 22:33:19 +0000

    Update

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace EADCA3
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("FootballAPI", client => client.BaseAddress = new Uri("https://api.football-data.org/v2/competitions/2021/standings?standingType=TOTAL"));
 
             await builder.Build().RunAsync();
         }
