7e30ad28ebd5d9edb054e64d6ff96612b8a0084f
ryandeering <ryandeering1@gmail.com>
2020-12-09 19:26:56 +0000

    Getting rid of debug lines

--- ServiceExtensions.cs
+++ ServiceExtensions.cs
@@ -19,25 +19,16 @@ namespace EADCA3
         public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url)
         {
             var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Add("X-Auth-Token", "e770fa76286949a19bc22fb912045957");
-
-           // request.Headers.Add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
-           // request.Headers.Add("Access-Control-Allow-Credentials", "true");
- 
-
-
+            request.Headers.Add("X-Auth-Token", "e770fa76286949a19bc22fb912045957"); 
+                        
             var response = await httpClient.SendAsync(request);
             response.Headers.Add("Access-Control-Allow-Origin", "*");
             response.Headers.Add("X-Auth-Token", "e770fa76286949a19bc22fb912045957");
             response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-
 
             var responseBytes = await response.Content.ReadAsByteArrayAsync();
-            var deserializedResponse = JsonSerializer.Deserialize<T>(responseBytes);
 
 
-            // return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
             return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { });
         }
     }
