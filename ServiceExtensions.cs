e720cb783463bf0691f9d1fdeab7671723936011
Ryan Deering <37181720+ryandeering@users.noreply.github.com>
2020-12-09 11:50:48 +0000

    Update ServiceExtensions.cs

--- ServiceExtensions.cs
+++ ServiceExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -16,15 +16,25 @@ namespace EADCA3
     public static class ServiceExtensions
     {
 
-        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url, AuthenticationHeaderValue authorization)
+        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url)
         {
             var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Authorization = authorization;
-            request.Headers.Add("Access-Control-Allow-Headers", "*");
+            request.Headers.Add("X-Auth-Token", "e770fa76286949a19bc22fb912045957");
+
+           // request.Headers.Add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+           // request.Headers.Add("Access-Control-Allow-Credentials", "true");
+ 
+
 
             var response = await httpClient.SendAsync(request);
+
+            // response.Headers.Add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+            // response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("X-Auth-Token", "e770fa76286949a19bc22fb912045957");
             var responseBytes = await response.Content.ReadAsByteArrayAsync();
-            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            // return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { });
         }
     }
 }
