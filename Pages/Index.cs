d1933a60f88126cd95613b7318cc02d336e438f4
ryandeering <ryandeering1@gmail.com>
2020-12-12 15:36:50 +0000

    Messing with date input

--- Pages/Index.cs
+++ Pages/Index.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using EADCA3.Shared;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -31,75 +32,6 @@ namespace EADCA3.Pages
         {
             await GetDataAsync();
         }
-
-
-        public class Root
-        {
-            public Filters filters { get; set; }
-            public Competition competition { get; set; }
-            public Season season { get; set; }
-            public List<Standing> standings { get; set; }
-        }
-
-        public class Area
-        {
-            public int id { get; set; }
-            public string name { get; set; }
-        }
-
-        public class Competition
-        {
-            public int id { get; set; }
-            public Area area { get; set; }
-            public string name { get; set; }
-            public string code { get; set; }
-            public string plan { get; set; }
-            public DateTime lastUpdated { get; set; }
-        }
-
-        public class Season
-        {
-            public int id { get; set; }
-            public string startDate { get; set; }
-            public string endDate { get; set; }
-            public int currentMatchday { get; set; }
-            public object winner { get; set; }
-        }
-
-        public class Team
-        {
-            public int id { get; set; }
-            public string name { get; set; }
-            public string crestUrl { get; set; }
-        }
-
-        public class Table
-        {
-            public int position { get; set; }
-            public Team team { get; set; }
-            public int playedGames { get; set; }
-            public string form { get; set; }
-            public int won { get; set; }
-            public int draw { get; set; }
-            public int lost { get; set; }
-            public int points { get; set; }
-            public int goalsFor { get; set; }
-            public int goalsAgainst { get; set; }
-            public int goalDifference { get; set; }
-        }
-
-        public class Standing
-        {
-            public string stage { get; set; }
-            public string type { get; set; }
-            public object group { get; set; }
-            public List<Table> table { get; set; }
-        }
-
-        public class Filters
-        {
-        }
-
 
     }
 }
--- Pages/MatchesToday.cs
+++ Pages/MatchesToday.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EADCA3.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Threading.Tasks;
 
 namespace EADCA3.Pages
@@ -11,7 +12,7 @@ namespace EADCA3.Pages
         private DateTime Date = DateTime.Now;
         private string ErrorMessage;
         private string League = "PL";
-
+        public EventCallback<DateTime> DateValueChanged{ get; set; }
         private async Task GetDataAsync()
         {
             try
@@ -27,117 +28,15 @@ namespace EADCA3.Pages
             }
         }
 
-
         protected override async Task OnInitializedAsync()
         {
             await GetDataAsync();
         }
 
-
-        public class Root
+        private async Task OnValueChanged()
         {
-            public int count { get; set; }
-            public Filters filters { get; set; }
-            public Competition competition { get; set; }
-            public List<Match> matches { get; set; }
+            await GetDataAsync();
         }
 
-   
-
-        public class Area
-        {
-            public int id { get; set; }
-            public string name { get; set; }
-        }
-
-        public class Competition
-        {
-            public int id { get; set; }
-            public Area area { get; set; }
-            public string name { get; set; }
-            public string code { get; set; }
-            public string plan { get; set; }
-            public DateTime lastUpdated { get; set; }
-        }
-
-        public class Season
-        {
-            public int id { get; set; }
-            public string startDate { get; set; }
-            public string endDate { get; set; }
-            public int currentMatchday { get; set; }
-        }
-
-        public class Odds
-        {
-            public string msg { get; set; }
-        }
-
-        public class FullTime
-        {
-            public object homeTeam { get; set; }
-            public object awayTeam { get; set; }
-        }
-
-        public class HalfTime
-        {
-            public object homeTeam { get; set; }
-            public object awayTeam { get; set; }
-        }
-
-        public class ExtraTime
-        {
-            public object homeTeam { get; set; }
-            public object awayTeam { get; set; }
-        }
-
-        public class Penalties
-        {
-            public object homeTeam { get; set; }
-            public object awayTeam { get; set; }
-        }
-
-        public class Score
-        {
-            public object winner { get; set; }
-            public string duration { get; set; }
-            public FullTime fullTime { get; set; }
-            public HalfTime halfTime { get; set; }
-            public ExtraTime extraTime { get; set; }
-            public Penalties penalties { get; set; }
-        }
-
-        public class HomeTeam
-        {
-            public int id { get; set; }
-            public string name { get; set; }
-        }
-
-        public class AwayTeam
-        {
-            public int id { get; set; }
-            public string name { get; set; }
-        }
-
-        public class Match
-        {
-            public int id { get; set; }
-            public Season season { get; set; }
-            public DateTime utcDate { get; set; }
-            public string status { get; set; }
-            public int matchday { get; set; }
-            public string stage { get; set; }
-            public string group { get; set; }
-            public DateTime lastUpdated { get; set; }
-            public Odds odds { get; set; }
-            public Score score { get; set; }
-            public HomeTeam homeTeam { get; set; }
-            public AwayTeam awayTeam { get; set; }
-            public List<object> referees { get; set; }
-        }
-       
-        public class Filters
-        {
-        }
     }
 }
