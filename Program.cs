6db00850faf5d3739e963252f1ba70a6936a5375
ryandeering <ryandeering1@gmail.com>
2020-12-12 17:47:48 +0000

    Converted to .NET 5

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EADCA3
@@ -11,7 +15,7 @@ namespace EADCA3
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
