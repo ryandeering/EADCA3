fa3a35ea48f6f8b983519aa826eeeb669cd93e6a
ryandeering <ryandeering1@gmail.com>
2020-12-12 21:22:01 +0000

    final restructure

--- EADCA3/GlobalSuppressions.cs
+++ EADCA3/GlobalSuppressions.cs
@@ -5,8 +5,6 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "Client-side environment variables don't make such sense, do they now? The Blazor developers seem to agree.", Scope = "member", Target = "~M:EADCA3.Pages.Indext.GetDataAsync~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "Client-side environment variables don't make such sense, do they now? The Blazor developers seem to agree.", Scope = "member", Target = "~M:EADCA3.Pages.Index.GetDataAsync~System.Threading.Tasks.Task")]
 [assembly: SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "Impossible to fix with Blazor WebAssembly. Literally the default.", Scope = "type", Target = "~T:EADCA3.Program")]
-[assembly: SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "Client-side environment variables don't make such sense, do they now? The Blazor developers seem to agree.", Scope = "member", Target = "~M:EADCA3.Pages.MatchesTodayt.GetDataAsync~System.Threading.Tasks.Task")]
-[assembly: SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "<Pending>", Scope = "member", Target = "~M:EADCA3.Pages.Index.GetDataAsync~System.Threading.Tasks.Task")]
-[assembly: SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "<Pending>", Scope = "member", Target = "~M:EADCA3.Pages.MatchesToday.GetDataAsync~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "Client-side environment variables don't make such sense, do they now? The Blazor developers seem to agree.", Scope = "member", Target = "~M:EADCA3.Pages.MatchesToday.GetDataAsync~System.Threading.Tasks.Task")]
