1774789b618ff7de38bc1d6f74401c673fa97faf
Ryan Deering <ryandeering1@gmail.com>
2020-12-14 16:13:16 +0000

    add readme.md

--- CA3Tests/UnitTest1.cs
+++ CA3Tests/UnitTest1.cs
@@ -60,6 +60,27 @@ namespace CA3Tests
 
         }
 
+        [Fact]
+        public async Task TestParseFail()
+        {
+            var Http = new HttpClient();
+
+            string teststring = "{\"count\":1,\"filters\":{},\"competition\":{\"id\":2021,\"area\":{\"id\":2072,\"name\":\"England\"},\"name\":\"Premier League\",\"code\":\"PL\",\"plan\":\"TIER_ONE\",\"lastUpdated\":\"2020-12-12T19:56:23Z\"},\"matches\":[{\"id\":303874,\"season\":{\"id\":619,\"startDate\":\"2020-09-12\",\"endDate\":\"2021-05-23\",\"currentMatchday\":12},\"utcDate\":\"2020-12-11T20:00:00Z\",\"status\":\"FINISHED\",\"matchday\":12,\"stage\":\"REGULAR_SEASON\",\"group\":\"Regular Season\",\"lastUpdated\":\"2020-12-12T00:55:18Z\",\"odds\":{\"msg\":\"Activate Odds-Package in User-Panel to retrieve odds.\"},\"score\":{\"winner\":\"AWAY_TEAM\",\"duration\":\"REGULAR\",\"fullTime\":{\"homeTeam\":1,\"awayTeam\":2},\"halfTime\":{\"homeTeam\":1,\"awayTeam\":1},\"extraTime\":{\"homeTeam\":null,\"awayTeam\":null},\"penalties\":{\"homeTeam\":null,\"awayTeam\":null}},\"homeTeam\":{\"id\":341,\"name\":\"Leeds United FC\"},\"awayTeam\":{\"id\":563,\"name\":\"West Ham United FC\"},\"referees\":[{\"id\":11605,\"name\":\"Michael Oliver\",\"role\":\"MAIN_REFEREE\",\"nationality\":\"England\"},{\"id\":11564,\"name\":\"Stuart Burt\",\"role\":\"ASSISTANT_N1\",\"nationality\":\"England\"},{\"id\":11488,\"name\":\"Simon Bennett\",\"role\":\"ASSISTANT_N2\",\"nationality\":\"England\"},{\"id\":11469,\"name\":\"Darren England\",\"role\":\"FOURTH_OFFICIAL\",\"nationality\":\"England\"}]}]}";
+            Root test = JsonSerializer.Deserialize<Root>(teststring);
+            Root test2;
+            DateTime date = DateTime.Parse("2020-12-13");
+            String dateParsed = date.ToString("yyyy-MM-dd");
+
+            string uri = "https://api.football-data.org/v2/competitions/" + "PL" + "/matches?dateFrom=" + dateParsed + "&dateTo=" + dateParsed;
+
+            test2 = await Http.GetJsonAsync<Root>(uri);
+
+            await Task.Delay(2000);
+
+
+            Assert.NotEqual(test.matches[0].homeTeam.name, test2.matches[0].homeTeam.name);
+
+        }
 
 
 
---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Enterprise Application Development: Continous Assessment 3
+
+Check it out at: http://eadca3-1-dev-as.azurewebsites.net/
+
+### What is it?
+This CA was a single page application using .NET 5 Blazor WebAssembly, and Bunit and Xunit for unit tests.
+It queries a <a href ="https://www.football-data.org/documentation/api"> football-data.org's API for domestic football (soccer) league information, like the current standings and matches on the current day.
+There are filters for the different European leagues and the date, so a user can view the matches that might've happened on a certain day.
+It is deployed on Azure App Engine using the link above. 
+
+### Screenshots
+
+<img width="1000" alt="Index" src="documentation/1.jpg">
+<br><br>
+<img width="1000" alt="Index" src="documentation/2.jpg">
